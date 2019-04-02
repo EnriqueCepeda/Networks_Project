e14f570fd2627b614eef0c40800f5e398f1dfab6
Enrique Cepeda <ecepedavillamayor@gmail.com>
2019-04-02 20:23:14 +0200

    TFTP_Client improved, only remains control error messages and ack

--- TFTP_UDPClient.py
+++ TFTP_UDPClient.py
@@ -14,7 +14,7 @@ def write(sock,ip,port,*args,**kwargs):
         raise Exception("The number of arguments is not correct")
 
     recieved=False
-    sock.sendto(struct.pack(f"!H{len(args[0])}sB{len('netascii')}sB",2,args[0],0,b'netascii',0), (ip,port) ) 
+    sock.sendto(struct.pack(f"!H{len(args[0])}sB{len('netascii')}sB",2,args[0].encode(),0,b'netascii',0), (ip,int(port)) ) 
     #Enviamos el paquete para empezar a leer del servidor , args[0] es el nombre del archivo de texto que queremos leer
     #! es por el formato de la red
     #H es por un unsigned short de 2 Bytes
@@ -33,34 +33,30 @@ def write(sock,ip,port,*args,**kwargs):
                     #Podemos recibir una RRQ/ERR o normalmente un ACK de tamaño 4 que es el tamaño del acknowledgmente que serían 2 bytes del codigo y otros 2 del codigo
                     msg,cliente = sock.recvfrom(516)
 
-                    unpacked_code = struct.unpack(f'H{len(msg)-2}s', msg) #Extraemos todo el paquete del servidor dividiendolo en codigo de mensaje y lo demás para primero analizar el codigo
+                    unpacked_code = struct.unpack(f'=H{len(msg)-2}s', msg) #Extraemos todo el paquete del servidor dividiendolo en codigo de mensaje y lo demás para primero analizar el codigo
 
                     #En este caso el codigo sería el correcto, el codigo 4, que significa que hemos recibido el OK del server, un acknowledgment
                     if(unpacked_code[0]==4):
 
                         sent_bytes=f.read(512)
 
-                        unpacked_data = struct.unpack('H', unpacked_code[1])
+                        unpacked_data = struct.unpack('=H', unpacked_code[1])
                         
                         #EL PRIMER MENSAJE DE ACK DEL SERVER AL CLIENTE TIENE QUE TENER BLOQUE 0
-                        sock.sendto(struct.pack(f'=2H{len(sent_bytes)}', 4 , unpacked_data[1]+1 ,sent_bytes) , (ip,port) )   
-
-
-
-                      
-
+                        sock.sendto(struct.pack(f'!2H{len(sent_bytes)}', 4 , unpacked_data[1]+1 ,sent_bytes) , (ip,port) )   
+                    
                     else:
+                        #TODAVIA NO VA ESTA PARTE
                         #En este caso estamos recibiendo el codigo 5, que significa que ha habido un error en el servidor, tendríamos que mostrar un mensaje al usuario
 
                         #Lo dividimos en 2 bytes de error, el mensaje, y un byte que es un 0
-                        unpacked_data = struct.unpack(f'H{len(unpacked_code[1])-3}sB', unpacked_code[1])
+                        unpacked_data = struct.unpack(f'=H{len(unpacked_code[1])-3}sB', unpacked_code[1])
                         print(f"Error number:{unpacked_data[0]} Message:{unpacked_data[1]}")
                         break
                         
                         
-
                 else:
-                    sock.sendto(struct.pack(f'=2H{len(sent_bytes)}', 4 , unpacked_data[1]+1 ,sent_bytes) , (ip,port) )
+                    sock.sendto(struct.pack(f'!2H{len(sent_bytes)}', 4 , unpacked_data[1]+1 ,sent_bytes) , (ip,port) )
                     recieved=False
                         
                 
@@ -79,14 +75,9 @@ def read(sock,ip,port,*args,**kwargs):
         raise Exception("The number of arguments is not correct")
 
     acknowledgment=False
-    
-    sock.sendto(struct.pack(f"!H{len(args[0])}sB{len('netascii')}sB",1,args[0].encode(),0,b'netascii',0), (ip,port) ) 
-    #Enviamos el paquete para empezar a leer del servidor , args[0] es el nombre del archivo de texto que queremos leer
-    #! es por el formato de la red
-    #H es por un unsigned short de 2 Bytes
-    #s es por una cadena de caracteres cuya longitud esta definida por la propia cadena en el formato
-    #B es por un unsigned char que ocupa 1 Byte
 
+    estructura=struct.pack(f"!H{len(args[0])}sB{len('netascii')}sB",1,str.encode(args[0]),0,b'netascii',0)
+    sock.sendto(estructura, (ip ,int(port)))
 
     with open(args[0],'w',) as f:
 
@@ -99,29 +90,29 @@ def read(sock,ip,port,*args,**kwargs):
                     #516 es el tamaño de los 512 bytes de datos maximos mas los 2 bytes del codigo y los 2 bytes del bloque
                     msg,cliente = sock.recvfrom(516)
 
-                    unpacked_code = struct.unpack(f'H{len(msg)-2}s', msg) #Extraemos todo el paquete del servidor dividiendolo en codigo de mensaje y lo demás para primero analizar el codigo
+                    unpacked_code = struct.unpack(f'>H{len(msg)-2}s', msg) #Extraemos todo el paquete del servidor dividiendolo en codigo de mensaje y lo demás para primero analizar el codigo
 
                     #En este caso el codigo sería el correcto, el codigo 3, que significa que recibimos datos del servidor tftp
                     if(unpacked_code[0]==3):
 
-                        unpacked_data = struct.unpack(f'H{len(unpacked_code[1])-2}s', unpacked_code[1]) #Extraemos todo el paquete del servidor dividiendolo en los campos indicados en el comentario de arriba
+                        unpacked_data = struct.unpack(f'=H{len(unpacked_code[1])-2}s', unpacked_code[1]) #Extraemos todo el paquete del servidor dividiendolo en los campos indicados en el comentario de arriba
 
                         #Escribimos en un archivo que se llama igual que el archivo del server lo que recibimos de el
-                        f.write(unpacked_data[0])
+                        f.write(unpacked_data[1].decode("utf-8"))
                         
                         #Enviamos codigo 4 de ACK y el numero de bloque al que corresponde el acknowledgment
-                        sock.sendto(struct.pack('=2H', 4 , unpacked_data[1]) , cliente )   #ACK
+                        sock.sendto(struct.pack('!2H', 4 , unpacked_data[0]) , cliente )   #ACK
 
                     else:
+                        #TODAVIA NO VA ESTA PARTE
                         #En este caso estamos recibiendo el codigo 5, que significa que ha habido un error en el servidor, tendríamos que mostrar un mensaje al usuario
-
                         #Lo dividimos en 2 bytes de error, el mensaje, y un byte que es un 0
-                        unpacked_data = struct.unpack(f'H{len(unpacked_code[1])-3}sB', unpacked_code[1])
+                        unpacked_data = struct.unpack(f'=H{len(unpacked_code[1])-3}sB', unpacked_code[1])
                         print(f"Error number:{unpacked_data[0]} Message:{unpacked_data[1]}")
                         break
 
                 else:
-                    sock.sendto(struct.pack('=2H', 4 , unpacked_data[1]), cliente )   #ACK
+                    sock.sendto(struct.pack('!2H', 4 , unpacked_data[1]), cliente )   #ACK
                     acknowledgment=False
                         
                 
@@ -154,9 +145,8 @@ def main(*args,**kwargs):
             command = input('TFTP@UDP> ').lower()
             arguments = command.split()
             #sys.argv[0] es la ip // sys.argv[1] es el puerto //argumentos depende de la función que querramos invocar, cogemos apartir del 1 porque el 0 es el nombre del comando, READ, WRITE
-            functions[arguments[0]](sock,sys.argv[0],sys.argv[1],arguments[1:])
+            functions[arguments[0]](sock,sys.argv[1],sys.argv[2],arguments[1])
         except Exception:
-            print ('error')
             traceback.print_exc()
         finally:
             sock.close()
