5e78d9ab94e8f023d74d1ae1061d90685183da3a
gsergey99 <44723449+gsergey99@users.noreply.github.com>
2019-04-16 23:57:41 +0200

    Update TFTP_TCPClient.py

--- TFTP_TCPClient.py
+++ TFTP_TCPClient.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from socket import socket, AF_INET, SOCK_STREAM
+import socket
 import pickle
 import sys
 import struct
@@ -21,18 +21,14 @@ def write(sock,*args,**kwargs):
     sock.send(last_packet)
     
     block=1 
-    #Enviamos el paquete para empezar a leer del servidor , args[0] es el nombre del archivo de texto que queremos leer
-    #! es por el formato de la red
-    #H es por un unsigned short de 2 Bytes
-    #s es por una cadena de caracteres cuya longitud esta definida por la propia cadena en el formato
-    #B es por un unsigned char que ocupa 1 Byte
+
     
 
     with open(file_name,'r',) as f:
 
         autentication_packet=sock.recv(2)
         confirm_packet=struct.unpack(f'!H',autentication_packet)
-        print(confirm_packet[0])
+
         
         if (confirm_packet[0]==4):
             while True:         
@@ -55,16 +51,16 @@ def write(sock,*args,**kwargs):
             
             error_message=sock.recv(516)
             
-            error_packet=struct.unpack(f'=2H{len(error_message)-5}sB',error_message)
+            error_packet=struct.unpack(f'!2H{len(error_message)-5}sB',error_message)
                                 
-            print(f"Error number:{error_packet[0]} Message:",error_packet[2].decode() )
+            print(f"Error number:{error_packet[0]} Message:",error_packet[2].decode())
                  
             pass
     
         
 def read(sock,*args,**kwargs):
     if(len(args)!=4):
-        raise Exception("The number of arguments is not correct")
+        raise Exception("Please introduce the arguments in the correct format -> READ 'filename'")
     ip=args[2]
     port=args[3]
     file_name=args[0]
@@ -73,11 +69,7 @@ def read(sock,*args,**kwargs):
     last_packet=struct.pack(f"!H{len(file_name)}sB{len('netascii')}sB",1,str.encode(file_name),0,b'netascii',0)  
     
     sock.send(last_packet) 
-    #Enviamos el paquete para empezar a leer del servidor , args[0] es el nombre del archivo de texto que queremos leer
-    #! es por el formato de la red
-    #H es por un unsigned short de 2 Bytes
-    #s es por una cadena de caracteres cuya longitud esta definida por la propia cadena en el formato
-    #B es por un unsigned char que ocupa 1 Byte
+    
 
 
     with open(file_name,'wb',) as f:
@@ -86,35 +78,28 @@ def read(sock,*args,**kwargs):
             
                         
                 msg = sock.recv(516)         
-                    #516 es el tamaño de los 512 bytes de datos maximos mas los 2 bytes del codigo y los 2 bytes del bloque
+               
                 
-                
-                code_message = struct.unpack(f'=H{len(msg)-2}s',msg) #Extraemos todo el paquete del servidor dividiendolo en codigo de mensaje y lo demás para primero analizar el codigo
-                #En este caso el codigo sería el correcto, el codigo 3, que significa que recibimos datos del servidor tftp
-                              
+                code_message = struct.unpack(f'!H{len(msg)-2}s',msg)               
                 if(code_message[0]==3):
                                     
                     
-                    leftUnpackedMsg = struct.unpack(f'=H{len(code_message[1])-2}s', code_message[1]) #Extraemos todo el paquete del servidor dividiendolo en los campos indicados en el comentario de arriba
-                                        
-                        #Escribimos en un archivo que se llama igual que el archivo del server lo que recibimos de el
+                    leftUnpackedMsg = struct.unpack(f'!H{len(code_message[1])-2}s', code_message[1]) 
+
                     f.write(leftUnpackedMsg[1])
 
                     if(len(leftUnpackedMsg[1])<512):
                         
                         break
                                         
-                       #.decode('utf-8', 'backslashreplace')
+                       
                 
                 else:
-                   
-                        #En este caso estamos recibiendo el codigo 5, que significa que ha habido un error en el servidor, tendríamos que mostrar un mensaje al usuario
 
-                        #Lo dividimos en 2 bytes de error, el mensaje, y un byte que es un 0
                     leftUnpackedMsg= struct.unpack(f'!H{len(code_message[1])-3}sB',code_message[1])
                     
                     print(f"Error number:{code_message[0]} Message:{leftUnpackedMsg[1].decode()}")
-                        #depende de como lo haya hecho enrique lo tengo que mirar como lo ha hecho para el cliente proque teiene que ser concurrente
+
                     break
                 
     
@@ -131,16 +116,17 @@ functions={
 }
 
 def main(*args,**kwargs):
-
-    sock = socket(AF_INET, SOCK_STREAM)
-    sock.connect((sys.argv[1],int(sys.argv[2])))
-    while True:
-        command = input('TFTP@TCP> ').lower()
-        arguments = command.split() + sys.argv
-        functions[arguments[0]](sock,*arguments[1:])
-        
+    if(sys.argv[1]!='-s'or sys.argv[3]!='-p' or len(sys.argv) != 5 ):
+        raise Exception("Introduce the arguments in the correct format -> python3 TFTP_TCPClient.py -s 'server direction' -p 'port number' ")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    
+        sock.connect((sys.argv[2],int(sys.argv[4])))
+        while True:
+            command = input('TFTP@TCP> ').lower()
+            arguments = command.split() + sys.argv
+            functions[arguments[0]](sock,*arguments[1:])
             
-
+            
 
         
 
@@ -149,5 +135,5 @@ if __name__ == '__main__':
         sys.exit(main())
     except KeyboardInterrupt:
         pass
-    except Exception:
-        traceback.print_exc()
+    except Exception as e:
+        print(str(e))
